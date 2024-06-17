5e75e07e0ffe441d68961eb5443999e4b1eaadb7
Ian Barddal <136401651+IanBarddal@users.noreply.github.com>
2024-06-17 17:14:26 -0300

    Add files via upload

---
+++ Quantidade de Inteiros Diferentes/QuantidadeDeInteirosDiferentes.py
@@ -0,0 +1,12 @@
+quantidade = int (input ("Digite a quantidade de números a inserir: "))
+
+lista = set()
+
+contaNúmeros = 0
+
+for x in range (quantidade):
+    números = int(input("Digite os números: "))
+    lista.add(números)
+        
+print (", ".join(map(str, lista)))
+print (f"{len(lista)} números foram exibidos")
